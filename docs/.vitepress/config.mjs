8168cf2839fd54ee442acd5183b9b9f9564b8382
cimastone <531843376@qq.com>
2025-07-05 13:25:42 +0800

    remove ignore file && modify config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -3,6 +3,7 @@ import { withMermaid } from 'vitepress-plugin-mermaid'
 
 export default withMermaid(
   defineConfig({
+    base: "/blog/",
     title: "CimaStone's Blog",
     description: "基于 Docker 的个人博客",
     themeConfig: {
